ad5a676b8e8429b482ab33bff5ec6340137bad16
lifeprolific <thegregoman@gmail.com>
2018-09-11 14:28:29 -0400

    added sailor photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :questions
   has_many :answers
   has_many :votes
+  has_one_attached :avatar
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true
 end
