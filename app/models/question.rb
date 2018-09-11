0ce41184dd18042431beeec1e57fed85b8010c2d
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-09-11 15:39:28 -0400

    user permission updated

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
-  has_many :answers
+  has_many :answers, dependent: :destroy
   has_one :accepted_answer, :foreign_key => "question_accepted_id", :class_name => "Answer"
 
   def has_accepted_answer?
