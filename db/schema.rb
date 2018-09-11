2781b6921a6dd7328f5f807722822cea0ce40f6d
lifeprolific <thegregoman@gmail.com>
2018-09-10 20:00:11 -0400

    added votes model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_10_180418) do
+ActiveRecord::Schema.define(version: 2018_09_10_235900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,19 @@ ActiveRecord::Schema.define(version: 2018_09_10_180418) do
     t.index ["api_token"], name: "index_users_on_api_token", unique: true
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "value"
+    t.bigint "user_id"
+    t.bigint "answer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["answer_id"], name: "index_votes_on_answer_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
+  add_foreign_key "votes", "answers"
+  add_foreign_key "votes", "users"
 end
